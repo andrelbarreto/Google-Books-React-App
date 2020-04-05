6f82799fa39c031f29a88b92a64a67015c07276a
andrelbarreto <andrebarreto77@gmail.com>
2020-04-05 18:32:05 -0500

    Fix for Heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 }
 
 // Add routes, both API and view
